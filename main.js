f929cdbbf9a1ae8d65618b6c0ca22c01e491d41c
Nizakat Ali <nizakatali.official@gmail.com>
2026-08-08 07:56:14 +0500

    update Js Content

--- main.js
+++ main.js
@@ -119,3 +119,42 @@ if (contactForm) {
         contactForm.reset();
     });
 }
+
+// Contact section Connect with my Mail 
+
+const form = document.getElementById('form');
+const submitBtn = form.querySelector('button[type="submit"]');
+
+form.addEventListener('submit', async (e) => {
+    e.preventDefault();
+
+    const formData = new FormData(form);
+    formData.append("access_key", "14d9ec3e-3f37-49e8-b66d-bc0dbf2ee7f8");
+
+    const originalText = submitBtn.textContent;
+
+    submitBtn.textContent = "Sending...";
+    submitBtn.disabled = true;
+
+    try {
+        const response = await fetch("https://api.web3forms.com/submit", {
+            method: "POST",
+            body: formData
+        });
+
+        const data = await response.json();
+
+        if (response.ok) {
+            alert("Success! Your message has been sent.");
+            form.reset();
+        } else {
+            alert("Error: " + data.message);
+        }
+
+    } catch (error) {
+        alert("Something went wrong. Please try again.");
+    } finally {
+        submitBtn.textContent = originalText;
+        submitBtn.disabled = false;
+    }
+});
